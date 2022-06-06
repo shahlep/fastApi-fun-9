f412741e65e38d88e883019a8e930e35763a3284
Shah rahman <toshah.rahman@gmail.com>
2022-06-06 17:29:44 +0300

    fastApi fun 6

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ import uvicorn
 from fastapi import FastAPI
 from config.settings import settings
 
-app = FastAPI(title=settings.app_title,
-              description=settings.app_description,
-              openapi_url=settings.api_json_version,
-              version=settings.app_version,
-              openapi_tags=settings.tags,
-              )
+app = FastAPI(
+    title=settings.app_title,
+    description=settings.app_description,
+    openapi_url=settings.api_json_version,
+    version=settings.app_version,
+    openapi_tags=settings.tags,
+)
 
 
-@app.get("/user",tags=["User"])
+@app.get("/user", tags=["User"])
 def get_user():
     return {"message": "Hello User"}
 
 
-@app.get("/product",tags=["Product"])
+@app.get("/product", tags=["Product"])
 def get_product():
     return {"message": "Hello Product"}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app)
