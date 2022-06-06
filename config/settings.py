2e73aebf5bbcd876e8e0776549095cd687995c60
Shah rahman <toshah.rahman@gmail.com>
2022-06-06 17:27:31 +0300

    fastApi fun 5

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,11 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path("") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
 class Settings:
     app_title: str = "My Application"
     app_version: str = "0.1.1"
@@ -14,6 +22,12 @@ class Settings:
             "description": "Product Related Routes"
         }
     ]
+    POSTGRESS_USER: str = os.getenv("POSTGRESS_USER")
+    POSTGRESS_PASSWORD: str = os.getenv("POSTGRESS_PASSWORD")
+    POSTGRESS_SERVER: str = os.getenv("POSTGRESS_SERVER")
+    POSTGRESS_PORT: str = os.getenv("POSTGRESS_PORT")
+    POSTGRESS_DB: str = os.getenv("POSTGRESS_DB")
+    DATABASE_URL = f"postgresql://{POSTGRESS_USER}:{POSTGRESS_PASSWORD}@{POSTGRESS_SERVER}:{POSTGRESS_PORT}/{POSTGRESS_DB}"
 
 
 settings = Settings()
