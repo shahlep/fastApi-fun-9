5c2692215177bb703fb3b63a8a21fc30c0a06b5d
Shah rahman <toshah.rahman@gmail.com>
2022-06-06 18:43:13 +0300

    fastApi fun 10

---
+++ database.py
@@ -0,0 +1,15 @@
+from config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+# step 1 - create engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# step 2 - bind engine with session
+sessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+# step 3 - define declarative base for describing database tables
+Base = declarative_base()
+
+
