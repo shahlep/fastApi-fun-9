9c1e905177539ba5c69bf80de7dae36fcdc70e11
Shah rahman <toshah.rahman@gmail.com>
2022-06-07 17:22:47 +0300

    fastApi fun 18

--- hashing.py
+++ hashing.py
@@ -6,8 +6,8 @@ pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 class Hash:
     @staticmethod
     def get_hash_password(plain_password):
-        pass
+        return pwd_context.hash(plain_password)
 
     @staticmethod
     def verify_password(plain_password, hashed_password):
-        pass
+        return pwd_context.verify(plain_password,hashed_password)
