263a920feeccab2a1eb73d7540858e0d0dc4ec50
Shah rahman <toshah.rahman@gmail.com>
2022-06-08 17:45:35 +0300

    fastApi fun 45

--- routers/items.py
+++ routers/items.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from schemas import ItemCreate,ShowItem
+from schemas import ItemCreate, ShowItem
 from models import Items
 from sqlalchemy.orm import Session
 from database import get_db
@@ -8,11 +8,10 @@ from datetime import datetime
 router = APIRouter()
 
 
-@router.post("/item", tags=["Items"],response_model=ShowItem)
-def create_item(item: ItemCreate, db: Session = Depends(get_db)):
-    owner_id = 1
+@router.post("/item", tags=["Items"], response_model=ShowItem)
+def create_item(item: ItemCreate, user_id: int, db: Session = Depends(get_db)):
     date_posted = datetime.now().date()
-    item = Items(**item.dict(), date_posted=date_posted, owner_id=owner_id)
+    item = Items(**item.dict(), date_posted=date_posted, owner_id=user_id)
     db.add(item)
     db.commit()
     db.refresh(item)
