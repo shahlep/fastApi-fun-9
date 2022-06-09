6c0ddeac66ea4f8fc127ce7f22c4247183addf71
Shah rahman <toshah.rahman@gmail.com>
2022-06-09 06:24:48 +0300

    fastApi fun 63

--- tests/test_item_route.py
+++ tests/test_item_route.py
@@ -7,3 +7,10 @@ def test_create_item(client):
     assert response.status_code == 200
     assert response.json()["title"] == "test title"
     assert response.json()["description"] == "test description"
+
+
+def test_get_item_by_id(client):
+    response = client.get("/items/1")
+    assert response.status_code == 200
+    assert response.json()["title"] == "test title"
+    assert response.json()["description"] == "test description"
