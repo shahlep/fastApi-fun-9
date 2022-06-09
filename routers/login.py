c5a261e2537f223687ecfcd938d887a06b803239
Shah rahman <toshah.rahman@gmail.com>
2022-06-09 20:06:15 +0300

    fastApi fun 86

--- routers/login.py
+++ routers/login.py
@@ -3,6 +3,9 @@ from fastapi import APIRouter
 
 oauth_scheme = OAuth2PasswordBearer(tokenUrl="login/token")
 
-
 router = APIRouter()
 
+
+@router.post("/login/token", tags=["Login"])
+def get_token_after_authentication():
+    pass
