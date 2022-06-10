d88260a6f8fc196e22c5982054ad286c5e26088e
Shah rahman <toshah.rahman@gmail.com>
2022-06-10 20:14:29 +0300

    fastApi fun 114

--- tests/test_item_route.py
+++ tests/test_item_route.py
@@ -1,9 +1,9 @@
 import json
 
 
-def test_create_item(client,token_header):
+def test_create_item(client, token_header):
     data = {"title": "test title", "description": "test description"}
-    response = client.post("/items", json.dumps(data),headers=token_header)
+    response = client.post("/items", json.dumps(data), headers=token_header)
     assert response.status_code == 200
     assert response.json()["title"] == "test title"
     assert response.json()["description"] == "test description"
