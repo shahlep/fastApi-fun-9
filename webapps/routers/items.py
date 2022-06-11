0045b0b8badb4f8db15f5becdf239177d7a6a2aa
Shah rahman <toshah.rahman@gmail.com>
2022-06-11 09:40:32 +0300

    fastApi fun 128

--- webapps/routers/items.py
+++ webapps/routers/items.py
@@ -8,4 +8,4 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/")
 def items_home(request: Request):
-    return templates.TemplateResponse("item_home_page.html", {"request": request})
+    return templates.TemplateResponse("items_home_page.html", {"request": request})
