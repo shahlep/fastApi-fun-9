999f84c20ec210452f8e59112e2a500bed8ac568
Shah rahman <toshah.rahman@gmail.com>
2022-06-11 17:09:21 +0300

    fastApi fun 171

--- webapps/routers/items.py
+++ webapps/routers/items.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
-from models import Items
+from models import Items, User
 from sqlalchemy.orm import Session
 from database import get_db
 
@@ -20,6 +20,7 @@ def items_home(request: Request, db: Session = Depends(get_db)):
 @router.get("/details/{id}")
 def item_details(request: Request, id: int, db: Session = Depends(get_db)):
     item = db.query(Items).filter(Items.id == id).first()
+    user = db.query(User).filter(User.id == item.owner_id).first()
     return templates.TemplateResponse(
-        "item_details_page.html", {"request": request, "item": item}
+        "item_details_page.html", {"request": request, "item": item,"user":user}
     )
