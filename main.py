5fd1a7e69be8b352734e9334777c686568a2a18f
Shah rahman <toshah.rahman@gmail.com>
2022-06-11 10:01:20 +0300

    fastApi fun 133

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from config.settings import settings
 from database import engine
 from models import Base
 from routers import user, items, login
+from webapps.routers import items as _items
 
 
 Base.metadata.create_all(bind=engine)
@@ -19,6 +20,7 @@ app = FastAPI(
 app.include_router(user.router)
 app.include_router(items.router)
 app.include_router(login.router)
+app.include_router(_items.router)
 
 if __name__ == "__main__":
     uvicorn.run(app)
