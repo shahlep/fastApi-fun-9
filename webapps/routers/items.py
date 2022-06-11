2eeb3676ebf570039bd0ec307061b5812e865b27
Shah rahman <toshah.rahman@gmail.com>
2022-06-11 15:21:23 +0300

    fastApi fun 150

--- webapps/routers/items.py
+++ webapps/routers/items.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from models import Items
+from sqlalchemy.orm import Session
 
 router = APIRouter(include_in_schema=False)
 
@@ -7,5 +9,6 @@ templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/")
-def items_home(request: Request):
+def items_home(request: Request,db:Session):
+    items = db.query(Items).all()
     return templates.TemplateResponse("items_home_page.html", {"request": request})
