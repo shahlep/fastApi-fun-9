5801453e3c0b41df8d528946b425a80d4bda98a0
Shah rahman <toshah.rahman@gmail.com>
2022-06-12 12:30:02 +0300

    fastApi fun 217

--- webapps/routers/login.py
+++ webapps/routers/login.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Request
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
@@ -7,5 +7,6 @@ templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/login")
-def login():
-    pass
+def login(request:Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
