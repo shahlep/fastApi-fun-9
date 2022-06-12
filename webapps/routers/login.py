a4da74605e9d6cd0fe5e91c6349b8b41a483eef7
Shah rahman <toshah.rahman@gmail.com>
2022-06-12 13:33:52 +0300

    fastApi fun 225

--- webapps/routers/login.py
+++ webapps/routers/login.py
@@ -18,4 +18,10 @@ async def login(request: Request, db: Session = Depends(get_db)):
     form = await request.form()
     email = form.get("email")
     password = form.get("password")
-
+    errors = []
+    if not email:
+        errors.append("Please enter valid email!")
+        return templates.TemplateResponse("login.html", {"request": request, "errors": errors})
+    if len(password) > 6:
+        errors.append("Password should be at least 6 characters!")
+        return templates.TemplateResponse("login.html", {"request": request, "errors": errors})
