4428493f3128692bd43acc1105445e2858952f80
Shah rahman <toshah.rahman@gmail.com>
2022-06-13 06:59:06 +0300

    fastApi fun 259

--- webapps/routers/items.py
+++ webapps/routers/items.py
@@ -45,3 +45,9 @@ async def create_item(request: Request):
         return templates.TemplateResponse(
             "create_item_page.html", {"request": request, "errors": errors}
         )
+    token = request.cookies.get("access_token")
+    if token is None:
+        errors.append("Please login first to create an item")
+        return templates.TemplateResponse(
+            "create_item_page.html", {"request": request, "errors": errors}
+        )
