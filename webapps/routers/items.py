e2fb874bed4612670156c54a4ce077cb2913fe1c
Shah rahman <toshah.rahman@gmail.com>
2022-06-13 17:27:10 +0300

    fastApi fun 272

--- webapps/routers/items.py
+++ webapps/routers/items.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Depends,responses,status
+from fastapi import APIRouter, Request, Depends, responses, status
 from fastapi.templating import Jinja2Templates
 from models import Items, User
 from sqlalchemy.orm import Session
@@ -36,7 +36,7 @@ def create_item(request: Request):
 
 
 @router.post("/create-an-item")
-async def create_item(request: Request,db:Session=Depends(get_db)):
+async def create_item(request: Request, db: Session = Depends(get_db)):
     form = await request.form()
     title = form.get("title")
     description = form.get("description")
@@ -62,18 +62,27 @@ async def create_item(request: Request,db:Session=Depends(get_db)):
                 param, Settings.SECRET_KEY, algorithms=Settings.ALGORITHM
             )
             email = payload.get("sub")
-            user = db.query(User).filter(User.email==email).first()
+            user = db.query(User).filter(User.email == email).first()
             if user is None:
-                errors.append("You are not authenticated.Please login with valid credentials or Create Account.")
+                errors.append(
+                    "You are not authenticated.Please login with valid credentials or Create Account."
+                )
                 return templates.TemplateResponse(
                     "create_item_page.html", {"request": request, "errors": errors}
                 )
             else:
-                item = Items(title=title,description=description,date_posted=datetime.now().date(),owner_id=user.id)
+                item = Items(
+                    title=title,
+                    description=description,
+                    date_posted=datetime.now().date(),
+                    owner_id=user.id,
+                )
                 db.add(item)
                 db.commit()
                 db.refresh(item)
-                return responses.RedirectResponse(f"/detail/{item.id}",status_code=status.HTTP_302_FOUND)
+                return responses.RedirectResponse(
+                    f"/detail/{item.id}", status_code=status.HTTP_302_FOUND
+                )
     except Exception:
         errors.append("Something went wrong!")
         return templates.TemplateResponse(
