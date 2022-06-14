4475118f59fa9153e4bea201bd41dd49134f2aa8
Shah rahman <toshah.rahman@gmail.com>
2022-06-14 20:04:08 +0300

    fastApi fun 298

---
+++ utils.py
@@ -0,0 +1,48 @@
+from typing import Dict, Optional
+from fastapi.exceptions import HTTPException
+from fastapi.openapi.models import OAuth2
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.security.utils import get_authorization_scheme_param
+from starlette.requests import Request
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+""" 
+https://github.com/tiangolo/fastapi/blob/master/fastapi/security/oauth2.py
+copied the method and change the request point to cookies from header as
+we store our token in cookies
+"""
+
+
+class OAuth2PasswordBearer(OAuth2):
+    def __init__(
+            self,
+            tokenUrl: str,
+            scheme_name: Optional[str] = None,
+            scopes: Optional[Dict[str, str]] = None,
+            description: Optional[str] = None,
+            auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
+        super().__init__(
+            flows=flows,
+            scheme_name=scheme_name,
+            description=description,
+            auto_error=auto_error,
+        )
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        # authorization: str = request.headers.get("Authorization")
+        authorization: str = request.cookies.get("access_token")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        return param
