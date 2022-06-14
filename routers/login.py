8117a715cd738e08aaef49bcb9e63a278f9c90ba
Shah rahman <toshah.rahman@gmail.com>
2022-06-14 20:33:09 +0300

    fastApi fun 304

--- routers/login.py
+++ routers/login.py
@@ -1,5 +1,5 @@
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import APIRouter, Depends, HTTPException, status,Response
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
 from database import get_db
 from models import User
@@ -15,8 +15,10 @@ router = APIRouter()
 
 
 @router.post("/login/token", tags=["Login"])
-def get_token_after_authentication(response:Response,
-    form_data: OAuth2PasswordRequestForm = Depends(),db: Session = Depends(get_db)
+def get_token_after_authentication(
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
 ):
     user = db.query(User).filter(User.email == form_data.username).first()
     if not user:
@@ -31,7 +33,5 @@ def get_token_after_authentication(response:Response,
 
     data = {"sub": form_data.username}
     jwt_token = jwt.encode(data, Settings.SECRET_KEY, algorithm=Settings.ALGORITHM)
-    response.set_cookie(
-        key="access_token", value=f"Bearer {jwt_token}", httponly=True
-    )
+    response.set_cookie(key="access_token", value=f"Bearer {jwt_token}", httponly=True)
     return {"access_token": jwt_token, "token_type": "bearer"}
